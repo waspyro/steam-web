e7e0493d10786e9f2f90861982c28d742466ad8e
ximoske <ximoske@gmail.com>
2023-04-03 13:41:36 +0300

    add static urls routes

---
+++ src/assets/urls.ts
@@ -0,0 +1,6 @@
+export const uCommunity = 'https://steamcommunity.com'
+export const uStore = 'https://store.steampowered.com'
+export const uApi = 'https://api.steampowered.com'
+
+export const uInventory = uCommunity + '/inventory'
+export const uInventoryProfiles = uInventory + '/profiles'
