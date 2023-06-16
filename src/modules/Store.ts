2bc380344706786ddbc62e35c5df5515f9c1c447
ximoske <ximoske@gmail.com>
2023-06-17 00:21:34 +0300

    reindent

--- src/modules/Store.ts
+++ src/modules/Store.ts
@@ -2,16 +2,16 @@ import SteamWebModule from "./SteamWebModule";
 import {Numberable} from "../types";
 import {appDetails, bundlePage, packageDetails, storeSearch} from "../requests/storeRequests";
 import {
-    asJson,
-    asSuccessJson,
-    ExpectAndRun, getSuccessfullText,
-    statusOk
+	asJson,
+	asSuccessJson,
+	ExpectAndRun, getSuccessfullText,
+	statusOk
 } from "../utils/responseProcessors";
 import {
-    AppDetails,
-    AppDetailsFilters,
-    AppDetailsPriceOverviewResponse,
-    PackageDetails, StoreSearchParams, StoreSearchResponse
+	AppDetails,
+	AppDetailsFilters,
+	AppDetailsPriceOverviewResponse,
+	PackageDetails, StoreSearchParams, StoreSearchResponse
 } from "../types/storeTypes";
 import {BadJSONStatus, UnexpectedHTTPResponseStatus} from "../utils/errors";
 import {ECountry} from "../assets/ECurrency";
@@ -25,85 +25,84 @@ import StoreCart from "./StoreCart";
 
 export default class Store extends SteamWebModule {
 
-    constructor(steamWeb: SteamWeb) {
-        super(steamWeb)
-        this.#setDefaultStoreCookies()
-    }
+	constructor(steamWeb: SteamWeb) {
+		super(steamWeb)
+		this.#setDefaultStoreCookies()
+	}
 
-    private static SuccessfulJsonWithOneID = (id: Numberable) =>
-        ExpectAndRun(statusOk, asJson, (res: any) =>
-    {
-        id = String(id)
-        if(!res[id]) throw new MalformedResponse(res, id)
-        if(!res[id].success) throw new BadJSONStatus(res, [id.toString(), 'success'], [true], res[id]["success"])
-        return res[id].data
-    })
+	private static SuccessfulJsonWithOneID = (id: Numberable) =>
+		ExpectAndRun(statusOk, asJson, (res: any) => {
+			id = String(id)
+			if(!res[id]) throw new MalformedResponse(res, id)
+			if(!res[id].success) throw new BadJSONStatus(res, [id.toString(), 'success'], [true], res[id]["success"])
+			return res[id].data
+		})
 
-    getAppDetails(
-        appid: Numberable, countryCode?: ECountry, filters?: AppDetailsFilters[]
-    ): Promise<AppDetails> {
-        return this.request(false, appDetails, [appid], countryCode, filters)
-        (Store.SuccessfulJsonWithOneID(appid))
-    }
+	getAppDetails(
+		appid: Numberable, countryCode?: ECountry, filters?: AppDetailsFilters[]
+	): Promise<AppDetails> {
+		return this.request(false, appDetails, [appid], countryCode, filters)
+		(Store.SuccessfulJsonWithOneID(appid))
+	}
 
-    getAppsPriceOverview(
-        appids: Numberable[], countryCode?: ECountry
-    ): Promise<AppDetailsPriceOverviewResponse> {
-        return this.request(false, appDetails, appids, countryCode, ['price_overview'])
-        (ExpectAndRun(statusOk, asJson))
-    }
+	getAppsPriceOverview(
+		appids: Numberable[], countryCode?: ECountry
+	): Promise<AppDetailsPriceOverviewResponse> {
+		return this.request(false, appDetails, appids, countryCode, ['price_overview'])
+		(ExpectAndRun(statusOk, asJson))
+	}
 
-    packageDetails(packageid: Numberable, countyCode?: ECountry): Promise<PackageDetails> {
-        return this.request(false, packageDetails, packageid, countyCode)
-        (Store.SuccessfulJsonWithOneID(packageid))
-    }
+	packageDetails(packageid: Numberable, countyCode?: ECountry): Promise<PackageDetails> {
+		return this.request(false, packageDetails, packageid, countyCode)
+		(Store.SuccessfulJsonWithOneID(packageid))
+	}
 
-    bundleDetails(bundleid: Numberable): Promise<BundleDetailsParsed> {
-        return this.request(false, bundlePage, bundleid)
-        (res => res.text().then(html => {
-            if(res.status === 200) return parseStoreBundlePage(html, bundleid)
-            if(res.status === 302 && res.headers.get('location') === 'https://store.steampowered.com/')
-                throw new Error('Bundle '+bundleid+' not exists or not accessible')
-            throw new UnexpectedHTTPResponseStatus(res, [200])
-        }))
-    }
+	bundleDetails(bundleid: Numberable): Promise<BundleDetailsParsed> {
+		return this.request(false, bundlePage, bundleid)
+		(res => res.text().then(html => {
+			if(res.status === 200) return parseStoreBundlePage(html, bundleid)
+			if(res.status === 302 && res.headers.get('location') === 'https://store.steampowered.com/')
+				throw new Error('Bundle '+bundleid+' not exists or not accessible')
+			throw new UnexpectedHTTPResponseStatus(res, [200])
+		}))
+	}
 
-    search(params: StoreSearchParams): Promise<StoreSearchResponse & {results: ParsedStoreSearchResponse}> {
-        return this.request(false, storeSearch, params)
-        (ExpectAndRun(statusOk, asSuccessJson, (json: any) => { //StoreSearchResponse
-            json.results = parseStoreSearchResponse(json.results_html);
-            return json
-        }))
-    }
+	search(params: StoreSearchParams): Promise<StoreSearchResponse & {results: ParsedStoreSearchResponse}> {
+		return this.request(false, storeSearch, params)
+		(ExpectAndRun(statusOk, asSuccessJson, (json: any) => { //StoreSearchResponse
+			json.results = parseStoreSearchResponse(json.results_html);
+			return json
+		}))
+	}
 
-    createCart(id?: string) {
-        return new StoreCart(this.web, id)
-    }
+	createCart(id?: string) {
+		return new StoreCart(this.web, id)
+	}
 
-    addFreeLicense(subid: Numberable) {
-        return this.request(true, addFreeLicense, this.web.session.sessionid, subid)
-        (getSuccessfullText).then(text => {
-            if(!text.includes('<h2>Success!</h2>')) throw new Error('Failed to add free license to account')
-            return true
-        })
-    }
+	addFreeLicense(subid: Numberable) {
+		return this.request(true, addFreeLicense, this.web.session.sessionid, subid)
+		(getSuccessfullText).then(text => {
+			if(!text.includes('<h2>Success!</h2>')) throw new Error('Failed to add free license to account')
+			return true
+		})
+	}
 
-    #setDefaultStoreCookies() { //todo temporary cookies? use decorators?
-        const store = this.web.session.cookies
-        const url = new URL(uStore)
-        const cookies = store.get(url)
-        if(!cookies.get('birthtime')) store.add({
-            domain: url.hostname,
-            path: '/',
-            name: 'birthtime',
-            value: '880927201'
-        })
-        if(!cookies.get('lastagecheckage')) store.add({
-            domain: url.hostname,
-            path: '/',
-            name: 'lastagecheckage',
-            value: '1-0-1998'
-        })
-    }
+	#setDefaultStoreCookies() { //todo temporary cookies? use decorators?
+		const store = this.web.session.cookies
+		const url = new URL(uStore)
+		const cookies = store.get(url)
+		if(!cookies.get('birthtime')) store.add({
+			domain: url.hostname,
+			path: '/',
+			name: 'birthtime',
+			value: '880927201'
+		})
+		if(!cookies.get('lastagecheckage')) store.add({
+			domain: url.hostname,
+			path: '/',
+			name: 'lastagecheckage',
+			value: '1-0-1998'
+		})
+	}
 
 }
