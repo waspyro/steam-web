d8e0ace5dfaf1383ddacef25dc16e3467703adec
ximoske <ximoske@gmail.com>
2023-07-03 22:20:59 +0300

    add fee calc helper

---
+++ src/helpers/FeeCalculator.ts
@@ -0,0 +1,21 @@
+const DEFAULT_STEAM_FEE = 0.05
+const DEFAULT_APP_FEE = 0.10
+
+export const FeeCalculator = (appFee = DEFAULT_APP_FEE, steamFee = DEFAULT_STEAM_FEE) => {
+  const toSeller = (priceWithCents: number) => // 10_00 => 11_50
+    priceWithCents + (Math.floor(priceWithCents * steamFee) || 1) +
+                     (Math.floor(priceWithCents * appFee)   || 1)
+
+  const toBuyer = (priceWithCents: number) => { // 11_50 => 10_00; 10_00 => 8_70
+    let start = Math.floor(priceWithCents / (1 + steamFee + appFee))
+    while (++start < priceWithCents) {
+      const candidate = toSeller(start)
+      if(priceWithCents === candidate) return start
+      else if (priceWithCents < candidate) return start - 1
+    }
+  }
+
+  return {toSeller, toBuyer}
+  //i don't know how to name this accordingly (with/withoutFee, toUpper/Lover seems bad)
+
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import SteamWeb from "./SteamWeb";
 import {StartCountIt, StartCountItEvery, consumeIt} from "./helpers/startCountIterator";
 import NameIDCollector from "./helpers/NameidCollector";
 import {accountid2steamid, steamid2accountid} from "./utils";
-
+import {FeeCalculator} from "./helpers/FeeCalculator";
 export default SteamWeb
-export {StartCountIt, StartCountItEvery, consumeIt, NameIDCollector, steamid2accountid, accountid2steamid}
+export {StartCountIt, StartCountItEvery, consumeIt, NameIDCollector, steamid2accountid, accountid2steamid, FeeCalculator}
